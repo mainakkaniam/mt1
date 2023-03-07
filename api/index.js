318149d08d7e04c321ceed9c27e0bad7f5bcd6c8
mainak <heellomeelo@gmail.com>
2023-03-07 08:36:37 +0530

    final changes dn

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const Transaction = require('./models/Transaction.js');
 const mongoose = require('mongoose');
 
-const allowedOrigins = ['https://money-tracker-fwft.onrender.com']; 
+const allowedOrigins = ['http://localhost:3000']; 
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -20,11 +20,11 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Use cors middleware with the options
 
 app.use(express.json());
-app.get("https://money-tracker-fwft.onrender.com", (req, res) => {
+app.get('/api/test', (req, res) => {
     res.json('test ok3');
 })
 
-app.post("https://money-tracker-fwft.onrender.com", async (req, res) => {
+app.post('/api/transaction', async (req, res) => {
     console.log("mongodb+srv://mainak:jkeBTAvstcntro7i@cluster0.bcrbfvz.mongodb.net/?retryWrites=true&w=majority");
     await mongoose.connect(`mongodb+srv://mainak:jkeBTAvstcntro7i@cluster0.bcrbfvz.mongodb.net/?retryWrites=true&w=majority`);
     const { name, description, datetime, price } = req.body;
@@ -33,7 +33,7 @@ app.post("https://money-tracker-fwft.onrender.com", async (req, res) => {
     });
     res.json(transaction);
 });
-app.get("https://money-tracker-fwft.onrender.com", async (req, res) => {
+app.get('/api/transactions', async (req, res) => {
     await mongoose.connect(`mongodb+srv://mainak:jkeBTAvstcntro7i@cluster0.bcrbfvz.mongodb.net/?retryWrites=true&w=majority`);
     const transactions = await Transaction.find();
     res.json(transactions); 
